7e67dac4a2d3c2458109c53f5263d5d3746d5448
Thero6th <anthonyluis0188@gmail.com>
2020-01-27 19:10:20 -0500

    add characters

---
+++ characters/sketch.js
@@ -0,0 +1,12 @@
+/*
+characters sketch 1/27/2020
+*/
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+}
+
+// runs 60 fps draws graphics
+function draw() {
+	background('light blue');
+}
