24eb67673cb6e2f60148711ecace00a8d97ca8f0
Thero6th <48805682+Thero6th@users.noreply.github.com>
2020-01-29 18:59:28 -0500

    story

--- characters/sketch.js
+++ characters/sketch.js
@@ -2,11 +2,43 @@
 characters sketch 1/27/2020
 */
 
+// loading graphics
+var boozy;
+var drben;
+var b;
+
+// runs before set up
+function preload(){
+	boozy = loadImage('boozy.png');
+	drben = loadImage('drben.png');
+	b = loadImage('boozy.png');
+}
+
+//location variables
+
+var boozyX = 100;
+var boozyY = 300;
+
+var drbenX = 700;
+var drbenY = 280;
+
+var story = "Dr Ben has a friend named boozy; a woozy old fashioned donut"
+
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 }
 
 // runs 60 fps draws graphics
 function draw() {
-	background('light blue');
+	background('lightblue');
+
+	//draw characters
+	image(boozy, boozyX, boozyY);
+	image(drben, drbenX, drbenY);
+
+
+	// narration
+	textSize(50);
+	textAlign(CENTER, CENTER);
+	text(story, 300, 100, width/2);
 }
