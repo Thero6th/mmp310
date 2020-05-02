13ed5522c43872757e6fc72fcd48255620bc6890
Thero6th <48805682+Thero6th@users.noreply.github.com>
2020-05-01 23:58:04 -0400

    Add files via upload

--- 3d-sound/sketch.js
+++ 3d-sound/sketch.js
@@ -6,13 +6,18 @@
 */
 
 var music;
+var boils = [];
 
 function preload() {
-    music = loadSound('KieLoKaz.mp3');
+    music = loadSound('lobo_loco.mp3');
+
+    boils.push(loadSound("slime_1.wav"));
+    boils.push(loadSound("slime_2.wav"));
+    boils.push(loadSound("slime_3.wav"));
 }
 
 let particleSystem;
-var boxSize;
+var gooSize = 250;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
@@ -28,20 +33,28 @@ function keyPressed() {
             music.play();
         }
     }
+
+    if (keycode = 13) {
+        particleSystem.add();
+    }
 }
 
 function draw() {
-    background(20);
+    background(200);
 
     // drag to rotate up and down, scroll to zoom in and out
     orbitControl();
+
+    push();
+    stroke('lightgreen');
+    noFill();
+    box(gooSize);
+    pop();
 
     //stroke('lightgreen');
     //noFill();
     directionalLight(120, 120, 155, 1, 1, -1);
     pointLight(355, 355, 355, mouseX - width / 2, mouseY - height / 2, 150);
 
-
-    particleSystem.add();
     particleSystem.update();
 }
